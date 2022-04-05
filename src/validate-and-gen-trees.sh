caa78f1f0812d179072c3b71bc484b87423ac8c6
Joe Clarke <jclarke@cisco.com>
2022-04-05 14:22:57 -0400

    Adjust paths for validation of dependent modules.

--- src/validate-and-gen-trees.sh
+++ src/validate-and-gen-trees.sh
@@ -4,27 +4,26 @@
 # Does the user have all the IETF published models.
 #
 if [ ! -d ../../iana/yang-parameters ]; then
-   rsync -avz --delete rsync.iana.org::assignments/yang-parameters ../bin/iana/
+    rsync -avz --delete rsync.iana.org::assignments/yang-parameters ../bin/iana/
 fi
 
-for i in ../bin/ietf-*\@$(date +%Y-%m-%d).yang
-do
+for i in ../bin/ietf-*\@$(date +%Y-%m-%d).yang; do
     name=$(echo $i | cut -f 1-3 -d '.')
     echo "Validating $name.yang"
     if test "${name#^example}" = "$name"; then
-        response=`pyang --ietf --lint --strict --canonical -p ../bin/iana/yang-parameters -p ../bin/dependent -f tree --max-line-length=72 --tree-line-length=69 $name.yang > $name-tree.txt.tmp`
-    else            
-        response=`pyang --ietf --strict --canonical -p ../../../iana/yang-parameters -f tree --max-line-length=72 --tree-line-length=69 $name.yang > $name-tree.txt.tmp`
+        response=$(pyang --ietf --lint --strict --canonical -p ../bin/iana/yang-parameters -p ../bin/dependent -f tree --max-line-length=72 --tree-line-length=69 $name.yang >$name-tree.txt.tmp)
+    else
+        response=$(pyang --ietf --strict --canonical -p ../bin/iana/yang-parameters -f tree --max-line-length=72 --tree-line-length=69 $name.yang >$name-tree.txt.tmp)
     fi
     if [ $? -ne 0 ]; then
         printf "$name.yang failed pyang validation\n"
         printf "$response\n\n"
         echo
-	rm yang/*-tree.txt.tmp
+        rm yang/*-tree.txt.tmp
         exit 1
     fi
-    fold -w 71 $name-tree.txt.tmp > $name-tree.txt
-    response=`yanglint -p ../../../iana/yang-parameters $name.yang -i`
+    fold -w 71 $name-tree.txt.tmp >$name-tree.txt
+    response=$(yanglint -p ../bin/iana/yang-parameters $name.yang -i)
     if [ $? -ne 0 ]; then
         printf "$name.yang failed yanglint validation\n"
         printf "$response\n\n"
@@ -36,38 +35,36 @@ rm ../bin/*-tree.txt.tmp
 
 # Generate a sub-tree with a depth of 3
 
-for i in ../bin/ietf-syslog\@$(date +%Y-%m-%d).yang
-do
+for i in ../bin/ietf-syslog\@$(date +%Y-%m-%d).yang; do
     name=$(echo $i | cut -f 1-3 -d '.')
     echo "Generating abridged tree diagram for $name.yang"
     if test "${name#^example}" = "$name"; then
-       response=`pyang --lint --strict --canonical -p ../../../yang-parameters -f tree --tree-depth=3 --max-line-length=72 --tree-line-length=69 $name.yang > $name-sub-tree.txt.tmp`
-    else            
-        response=`pyang --ietf --strict --canonical -p ../../iana/yang-parameters -f tree --tree-depth=3 --max-line-length=72 --tree-line-length=69 $name.yang > $name-sub-tree.txt.tmp`
+        response=$(pyang --lint --strict --canonical -p ../bin/iana/yang-parameters -p ../bin/dependent -f tree --tree-depth=3 --max-line-length=72 --tree-line-length=69 $name.yang >$name-sub-tree.txt.tmp)
+    else
+        response=$(pyang --ietf --strict --canonical -p ../bin/iana/yang-parameters -f tree --tree-depth=3 --max-line-length=72 --tree-line-length=69 $name.yang >$name-sub-tree.txt.tmp)
     fi
     if [ $? -ne 0 ]; then
         printf "$name.yang failed generation of sub-tree diagram\n"
         printf "$response\n\n"
         echo
-	rm yang/*-sub-tree.txt.tmp
+        rm yang/*-sub-tree.txt.tmp
         exit 1
     fi
-    fold -w 71 $name-sub-tree.txt.tmp > $name-sub-tree.txt
+    fold -w 71 $name-sub-tree.txt.tmp >$name-sub-tree.txt
 done
 rm ../bin/*-sub-tree.txt.tmp
 
 echo "Validating examples"
 
 # Validate examples
-for i in yang/example-syslog-configuration-4.[1-2].xml
-do
+for i in yang/example-syslog-configuration-4.[1-2].xml; do
     name=$(echo $i | cut -f 1-3 -d '.')
     echo "Validating $name.xml"
-    response=`yanglint -ii -t config -p ../bin/iana/yang-parameters  ../bin/ietf-syslog\@$(date +%Y-%m-%d).yang $name.xml`
+    response=$(yanglint -ii -t config -p ../bin/iana/yang-parameters ../bin/ietf-syslog\@$(date +%Y-%m-%d).yang $name.xml)
     if [ $? -ne 0 ]; then
-       printf "failed (error code: $?)\n"
-       printf "$response\n\n"
-       echo
-       exit 1
+        printf "failed (error code: $?)\n"
+        printf "$response\n\n"
+        echo
+        exit 1
     fi
 done
